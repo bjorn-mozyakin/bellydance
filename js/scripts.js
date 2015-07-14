98b73c376405b6bc833e6e13f5391ebecd8c58ac
Rakov Dmitry <rakov.di@gmail.com>
2015-07-14 13:03:53 +0400

    Fix bugs and refactoring js and css for ImgGalleriesSlider

--- js/scripts.js
+++ js/scripts.js
@@ -1,173 +1,38 @@
 document.addEventListener('DOMContentLoaded', handler);
 
 function handler() {
-  var galleries =  document.querySelectorAll('.gallery');
-  if (!galleries) return;
 
-  [].forEach.call(galleries, function(gallery) {
-
-    var images = gallery.querySelectorAll('a');
-    [].forEach.call(images, function(img) {
-      (function(img) {
-        img.addEventListener('click', showImg);
-      })(img);
-    });
-
-  });
-
-  document.addEventListener('click', hideImg);
-  document.addEventListener('keydown', hideImgEsc);
-
+  /*Page Contacts - Form Question*/
   var question = document.querySelector('.question');
   if (question) {
     question.addEventListener('click', showForm);
   }
 
-  window.onresize = function() {
-    if (!document.querySelector('.fogging')) return;
+  var galleries =  document.querySelectorAll('.gallery');
+  if (galleries) {
 
-    var largeImg = document.querySelector('.large-img');
-    var arrows = document.querySelector('.arrows');
-    //var leftArrow = document.querySelector('.arrow-left');
-    //var rightArrow = document.querySelector('.arrow-right');
+    [].forEach.call(galleries, function(gallery) {
 
-    arrows.style.marginTop = largeImg.offsetHeight / 2 - parseFloat(getComputedStyle(arrows).height) - 10 + 'px';
+      var images = gallery.querySelectorAll('a');
+      [].forEach.call(images, function(img) {
+        (function(img) {
+          img.addEventListener('click', showImg);
+        })(img);
+      });
+
+    });
+
+    window.addEventListener('resize', resizeImg);
   }
 
-  function showImg(e) {
-    e = e || window.event;
-    var target = e.target || e.srcElement; //for IE8
-
-    document.body.style.overflow = "hidden";
-
-    //document.body.insertAdjacentHTML('beforeEnd', '<div class="fogging"></div>');
-    var href = target.getAttribute('href');
-    var str = ' \
-    <div class="fogging"> \
-      <img class="large-img" src="' + href + '"> \
-    </div> \
-    <div class="arrows"> \
-      <div class="arrow-left"></div> \
-      <div class="arrow-right"></div> \
-    </div> \
-    ';
-    document.body.insertAdjacentHTML('beforeEnd', str);
-
-    e.preventDefault();
-
-    var imgCount = target.parentNode.querySelectorAll('a').length;
-
-    var largeImg = document.querySelector('.large-img');
-    var arrows = document.querySelector('.arrows');
-    arrows.style.display = "none";
-
-    largeImg.onload = function() {
-      arrows.style.display = "";
-      arrows.style.marginTop = largeImg.offsetHeight / 2 - parseFloat(getComputedStyle(arrows).height) - 10 + 'px';
-    }
-
-    document.onkeydown = null;
-    // if (typeof(document.onkeydown)=="function") {
-    //   alert ('обработчик стоит');
-    // }
-    //document.removeEventListener("keydown", changeImg);
-      document.onkeydown = changeImg;
-
-
-
-
-    arrows.addEventListener("click", changeImg);
-
-    function changeImg(e) {
-      e = e || window.event;
-      var target = e.target || e.srcElement; //for IE8
-
-      var img = document.querySelector('.large-img');
-      var src = img.getAttribute('src');
-      var numberPos = src.lastIndexOf('.jpg') - 1;
-
-
-      if ((target.classList.contains('arrow-left')) || (e.keyCode = "37")) {
-        var newPos = +src.substr(numberPos, 1) - 1;
-
-        if (newPos < 1) {
-          newPos = imgCount;
-        }
-      }
-
-      if ((target.classList.contains('arrow-right')) || (e.keyCode = "39")) {
-        var newPos = +src.substr(numberPos, 1) + 1;
-
-        if (newPos > imgCount) {
-          newPos = 1;
-        }
-      }
-
-      var newsrc = src.slice(0, numberPos) + newPos + src.slice(numberPos + 1);
-      img.setAttribute('src', newsrc);
-    }
-
-    function nextImg(e) {
-      e = e || window.event;
-      var target = e.target || e.srcElement; //for IE8
-
-      var img = document.querySelector('.large-img');
-      var src = img.getAttribute('src');
-      var numberPos = src.lastIndexOf('.jpg') - 1;
-
-      var newPos = +src.substr(numberPos, 1) + 1;
-
-      if (newPos > imgCount) {
-        newPos = 1;
-      }
-
-      var newsrc = src.slice(0, numberPos) + newPos + src.slice(numberPos + 1);
-      img.setAttribute('src', newsrc);
-    }
-  }
-
-  function hideImgEsc(e) {
-    e = e || window.event;
-    var target = e.target || e.srcElement; //for IE8
-
-    if (e.keyCode == 27) {
-      if (!document.querySelector('.fogging')) return;
-
-      document.querySelector('.fogging').remove();
-      //document.querySelector('.container').remove();
-      //document.querySelector('.large-img').remove();
-      document.querySelector('.arrows').remove();
-      //document.querySelector('.arrow-left').remove();
-      //document.querySelector('.arrow-right').remove();
-          //document.removeEventListener("keydown", changeImg);
-
-      document.body.style.overflow = "";
-    }
-  }
-
-  function hideImg(e) {
-    e = e || window.event;
-    var target = e.target || e.srcElement; //for IE8
-
-    if (target.classList.contains('fogging')) {
-      document.querySelector('.fogging').remove();
-      //document.querySelector('.container').remove();
-      //document.querySelector('.large-img').remove();
-      document.querySelector('.arrows').remove();
-      //document.querySelector('.arrow-left').remove();
-      //document.querySelector('.arrow-right').remove();
-      document.body.style.overflow = "";
-          //document.removeEventListener("keydown", changeImg);
-
-    }
-  }
+  document.addEventListener('click', hideAll);
+  document.addEventListener('keydown', hideAll);
 
   function showForm(e) {
     e = e || window.event;
     var target = e.target || e.srcElement; //for IE8
 
     document.body.style.overflow = "hidden";
-//    document.body.insertAdjacentHTML('beforeEnd', '<div class="fogging"></div>');
 
     var str = ' \
     <div class="fogging"> \
@@ -188,4 +53,100 @@ function handler() {
     ';
     document.body.insertAdjacentHTML('beforeEnd', str);
   }
+
+  function showImg(e) {
+    e = e || window.event;
+    var target = e.target || e.srcElement; //for IE8
+
+    e.preventDefault();
+
+    var imgCount = target.parentNode.querySelectorAll('a').length;
+
+    document.body.style.overflow = "hidden";
+
+    var href = target.getAttribute('href');
+    var str = ' \
+    <div class="fogging"> \
+      <img class="large-img" src="' + href + '"> \
+    </div> \
+    <div class="arrows"> \
+      <div class="arrow-left"></div> \
+      <div class="arrow-right"></div> \
+    </div> \
+    ';
+    document.body.insertAdjacentHTML('beforeEnd', str);
+
+    var largeImg = document.querySelector('.large-img');
+    var arrows = document.querySelector('.arrows');
+
+    arrows.style.display = "none";
+    largeImg.addEventListener('load', showArrows);
+
+    //document.removeEventListener("keydown", changeImg);
+    //document.addEventListener("keydown", changeImg);
+    document.onkeydown = null;
+    document.onkeydown = changeImg;
+
+    arrows.addEventListener("click", changeImg);
+
+    function changeImg(e) {
+      e = e || window.event;
+      var target = e.target || e.srcElement; //for IE8
+
+      if (e.keyCode && e.keyCode != '37' && e.keyCode != '39') return;
+
+      var img = document.querySelector('.large-img');
+      var src = img.getAttribute('src');
+      var numberPos = src.lastIndexOf('.jpg') - 1;
+
+      if ((target.classList.contains('arrow-left')) || (e.keyCode == "37")) {
+        var newPos = +src.substr(numberPos, 1) - 1;
+
+        if (newPos < 1) {
+          newPos = imgCount;
+        }
+      }
+
+      if ((target.classList.contains('arrow-right')) || (e.keyCode == "39")) {
+        var newPos = +src.substr(numberPos, 1) + 1;
+
+        if (newPos > imgCount) {
+          newPos = 1;
+        }
+      }
+
+      var newsrc = src.slice(0, numberPos) + newPos + src.slice(numberPos + 1);
+      img.setAttribute('src', newsrc);
+    }
+
+    function showArrows() {
+      arrows.style.display = "";
+      arrows.style.marginTop = largeImg.offsetHeight / 2 - parseFloat(getComputedStyle(arrows).height) - 10 + 'px';
+    }
+  }
+
+  function resizeImg() {
+    if (!document.querySelector('.large-img')) return;
+
+    var largeImg = document.querySelector('.large-img');
+    var arrows = document.querySelector('.arrows');
+
+    arrows.style.marginTop = largeImg.offsetHeight / 2 - parseFloat(getComputedStyle(arrows).height) - 10 + 'px';
+  }
+
+  function hideAll(e) {
+    e = e || window.event;
+    var target = e.target || e.srcElement; //for IE8
+
+    if (!document.querySelector('.fogging')) return;
+
+    if ((e.keyCode == 27) || (target.classList.contains('fogging'))) {
+      document.querySelector('.fogging').remove();
+      document.body.style.overflow = "";
+
+      if (!document.querySelector('.arrows')) return;
+      document.querySelector('.arrows').remove();
+    }
+  }
+
 }
