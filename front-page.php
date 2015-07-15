4afe6f5e3704e43c5127052ebb91067691c41eb1
Rakov Dmitry <rakov.di@gmail.com>
2015-07-15 23:14:34 +0400

    Make sidebars on the main page  -shadows and rotate

--- front-page.php
+++ front-page.php
@@ -7,17 +7,17 @@ Template Name: zrak-wp
   <aside class="sidebar-primary">
     <?php get_sidebar('primary'); ?>
 
-    <div class="sidebar-primary-reviews">
-      <h2>Отзывы</h2>
-      <?php $the_query = new WP_Query('category_name=Отзывы&orderby=rand&showposts=1'); ?>
-      <?php while ($the_query->have_posts() ) : $the_query->the_post(); ?>
-        <div class="sidebar-primary-review">
+    <div class="sidebar-primary-reviews-wrapper">
+      <div class="sidebar-primary-reviews">
+        <h2>Отзывы</h2>
+        <?php $the_query = new WP_Query('category_name=Отзывы&orderby=rand&showposts=1'); ?>
+        <?php while ($the_query->have_posts() ) : $the_query->the_post(); ?>
           <strong><?php the_title(); ?></strong>
           <?php the_content(); ?>
-        </div>
-      <?php endwhile; ?>
-      <?php wp_reset_postdata();?>
-      <p class="more"><a href="">Посмотреть остальные отзывы...</a></p>
+        <?php endwhile; ?>
+        <?php wp_reset_postdata();?>
+        <p class="more"><a href="">Посмотреть остальные отзывы...</a></p>
+      </div>
     </div>
   </aside>
   <main id="content" class="clearfix">
