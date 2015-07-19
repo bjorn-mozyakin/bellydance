c80821d51309b838566be67fb5c85bbb2176ed2e
Rakov Dmitry <rakov.di@gmail.com>
2015-07-19 11:44:55 +0400

    Add close-button to forms. Refactoring code for Forms/Img

--- js/scripts.js
+++ js/scripts.js
@@ -27,7 +27,7 @@ function handler() {
 
     });
 
-    window.addEventListener('resize', resizeGallery);
+    window.addEventListener('resize', positionNavElems);
   }
 
   document.addEventListener('click', hideAll);
@@ -41,22 +41,25 @@ function handler() {
 
     var str = ' \
     <div class="fogging"> \
-    <form name="question" class="form" method="POST" action="http://bellydance-samara1.ru/wp-content/themes/zrak-wp-dance/post.php" autocmplete> \
-      <h2>Задайте вопрос</h2> \
-      <label for="name" data-required="true">Ваше имя </label> \
-      <input id="name" type="text" name="name" placeholder="Например: Ольга" required> \
-      <label for="email"data-required="true">Ваш e-mail </label> \
-      <input id="email" type="email" name="email" placeholder="Например: ivanova@mail.ru" required> \
-      <label for="title">Тема</label> \
-      <input id="title" type="text" name="title" placeholder="Например: Вопрос о расписании"> \
-      <label for="message">Сообщение</label> \
-      <textarea id="message" name="message" rows="5" placeholder="Например: Будут ли занятия в следующий четверг?"></textarea> \
-      <input type="submit" name="submit"></input> \
-      <p> - поля, обязательные для заполнения</p> \
-    </form> \
+      <form name="question" class="form" method="POST" action="http://bellydance-samara1.ru/wp-content/themes/zrak-wp-dance/post.php" autocmplete> \
+        <h2>Задайте вопрос</h2> \
+        <label for="name" data-required="true">Ваше имя </label> \
+        <input id="name" type="text" name="name" placeholder="Например: Ольга" required> \
+        <label for="email"data-required="true">Ваш e-mail </label> \
+        <input id="email" type="email" name="email" placeholder="Например: ivanova@mail.ru" required> \
+        <label for="title">Тема</label> \
+        <input id="title" type="text" name="title" placeholder="Например: Вопрос о расписании"> \
+        <label for="message">Сообщение</label> \
+        <textarea id="message" name="message" rows="5" placeholder="Например: Будут ли занятия в следующий четверг?"></textarea> \
+        <input type="submit" name="submit"></input> \
+        <p> - поля, обязательные для заполнения</p> \
+        <div class="close"></div> \
+      </form> \
     </div> \
     ';
     document.body.insertAdjacentHTML('beforeEnd', str);
+
+    positionNavElems();
   }
 
   function showFormBid(e) {
@@ -67,31 +70,32 @@ function handler() {
 
     var str = ' \
     <div class="fogging"> \
-    <form name="bid" class="form" method="POST" action="http://bellydance-samara1.ru/wp-content/themes/zrak-wp-dance/post.php" autocmplete> \
-      <h2>Заполните форму</h2> \
-      <label for="name" data-required="true">Ваше имя </label> \
-      <input id="name" type="text" name="name" placeholder="Например: Ольга" required> \
-      <label for="tel" data-required="true">Контактный телефон </label> \
-      <input id="tel" type="tel" name="email" placeholder="Например: 927-000-11-22" required> \
-      <label for="dancestyle" data-required="true">Выберите направление</label> \
-      <select id="dancestyle" name="title" required> \
-        <option></option> \
-        <option>Восточные танцы</option> \
-        <option>Фитнес</option> \
-      </select> \
-      <label for="halls" data-required="true">Выберите адрес зала</label> \
-      <select id="halls" name="title" required> \
-        <option></option> \
-        <option>ул. Мориса Тореза, 103 А (Парк Победы)</option> \
-        <option>ул. Пушкина (Бр. Коростелевых), 280</option> \
-        <option>ул. Стара-Загора, 164</option> \
-        <option>пр-т. Металлургов, 75</option> \
-      </select> \
-      <label for="date">Выберите желаемую дату начала занятий</label> \
-      <input id="date" type="date" name="date" min="2015-07-14" step="2"> \
-      <input type="submit" name="submit"></input> \
-      <p> - поля, обязательные для заполнения</p> \
-    </form> \
+      <form name="bid" class="form" method="POST" action="http://bellydance-samara1.ru/wp-content/themes/zrak-wp-dance/post.php" autocmplete> \
+        <h2>Заполните форму</h2> \
+        <label for="name" data-required="true">Ваше имя </label> \
+        <input id="name" type="text" name="name" placeholder="Например: Ольга" required> \
+        <label for="tel" data-required="true">Контактный телефон </label> \
+        <input id="tel" type="tel" name="email" placeholder="Например: 927-000-11-22" required> \
+        <label for="dancestyle" data-required="true">Выберите направление</label> \
+        <select id="dancestyle" name="title" required> \
+          <option></option> \
+          <option>Восточные танцы</option> \
+          <option>Фитнес</option> \
+        </select> \
+        <label for="halls" data-required="true">Выберите адрес зала</label> \
+        <select id="halls" name="title" required> \
+          <option></option> \
+          <option>ул. Мориса Тореза, 103 А (Парк Победы)</option> \
+          <option>ул. Пушкина (Бр. Коростелевых), 280</option> \
+          <option>ул. Стара-Загора, 164</option> \
+          <option>пр-т. Металлургов, 75</option> \
+        </select> \
+        <label for="date">Выберите желаемую дату начала занятий</label> \
+        <input id="date" type="date" name="date" min="2015-07-14" step="2"> \
+        <input type="submit" name="submit"></input> \
+        <p> - поля, обязательные для заполнения</p> \
+        <div class="close" title="Закрыть форму"></div> \
+      </form> \
     </div> \
     ';
     document.body.insertAdjacentHTML('beforeEnd', str);
@@ -99,9 +103,9 @@ function handler() {
     var danceStyle = document.getElementById('dancestyle');
     danceStyle.addEventListener('change', filterHalls);
 
-    function filterHalls() {
-        //select.selectedIndex;
+    positionNavElems();
 
+    function filterHalls() {
       var bid = document.forms.bid;
       var halls = bid.elements.halls;
 
@@ -118,7 +122,6 @@ function handler() {
       }
     }
   }
-
 
   function showImg(e) {
     e = e || window.event;
@@ -193,23 +196,34 @@ function handler() {
       //arrows.style.marginTop = largeImg.offsetHeight / 2 - parseFloat(getComputedStyle(arrows).height) - 10 + 'px';
       //arrows.style.marginLeft = 0 - largeImg.clientWidth / 2 + 'px';
       //arrows.style.width = largeImg.clientWidth + 'px';
-      resizeGallery();
+      positionNavElems();
     }
   }
 
-  function resizeGallery() {
-    if (!document.querySelector('.large-img')) return;
+  function positionNavElems() {
+    if (!document.querySelector('.fogging')) return;
 
     var largeImg = document.querySelector('.large-img');
     var close = document.querySelector('.close');
     var arrows = document.querySelector('.arrows');
+    var form = document.querySelector('.form');
 
-    close.style.marginLeft = largeImg.clientWidth / 2 - parseInt(getComputedStyle(close).width) + 'px';
-    close.style.marginTop = 0 - largeImg.clientHeight / 2 + 'px';
+    var mainElem;
 
-    arrows.style.marginLeft = 0 - largeImg.clientWidth / 2 + 'px';
-    arrows.style.marginTop = 0 - parseInt(getComputedStyle(arrows).height) / 2 + 'px';
-    arrows.style.width = largeImg.clientWidth + 'px';
+    if (largeImg) {
+      arrows.style.marginLeft = 0 - largeImg.clientWidth / 2 + 'px';
+      arrows.style.marginTop = 0 - parseInt(getComputedStyle(arrows).height) / 2 + 'px';
+      arrows.style.width = largeImg.clientWidth + 'px';
+
+      mainElem = largeImg;
+    }
+
+    if (form) {
+      mainElem = form;
+    }
+
+    close.style.marginLeft = mainElem.clientWidth / 2 - parseInt(getComputedStyle(close).width) + 'px';
+    close.style.marginTop = 0 - mainElem.clientHeight / 2 + 'px';
   }
 
   function hideAll(e) {
