6e4a8ebc2225d6a2aaef192b256ded1be1733260
Rakov Dmitry <rakov.di@gmail.com>
2015-08-16 13:56:45 +0300

    Add Google and Yandex analytics

--- header.php
+++ header.php
@@ -9,9 +9,6 @@
 <!--Disable auto scale on mobile devices-->
 <meta name="viewport" content="initial-scale=1" />
 
-<!--Point VK and FB thumbnail for displaying-->
-<!--<meta property="og:image" content="http://tamdaleko.ru/wp-content/themes/zrak-wp/images/logo-socnet.png" />-->
-
 <link rel="shortcut icon" type="image/png" href="<?php bloginfo('template_url'); ?>/favicon.ico" />
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/normalize.css" />
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/style.css" />
@@ -26,8 +23,49 @@
   <script src="//cdn.polyfill.io/v1/polyfill.min.js"></script>
   <![endif]-->
 
-
 <script src="<?php bloginfo('template_url'); ?>/js/scripts.js"></script>
+
+<!-- Google Analytics BEGIN -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-57266940-1', 'auto');
+  ga('send', 'pageview');
+</script>
+<!-- END Google Analytica -->
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter25984036 = new Ya.Metrika({
+                    id:25984036,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/25984036" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 
 </head>
 <body>
